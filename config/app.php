397e9794316034b8a5b5ff296c63135c424d2543
henryaugusta <henryaugusta4@gmail.com>
2021-08-28 12:37:23 +0700

    JWT Config #1 - Install Libraries from Tymon, add addition to config.php Implements changes on User.php,

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -227,6 +228,10 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class,
+        'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
+        // php artisan vendor:publish --provider="Tymon\JWTAuth\Providers\LaravelServiceProvider"
 
     ],
 
